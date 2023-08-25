30a69fba14d04d9f9e83d990732a56091822efd6
Madhin Hussain <madhin.hussain@newlook.com>
2023-08-25 13:38:46 +0100

    finished

--- expenses-tracker/src/components/Expenses/Expenses.js
+++ expenses-tracker/src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2023')
@@ -14,8 +15,8 @@ const Expenses = (props) => {
   }
 
   const filteredExpenses = props.items.filter((expense) => {
-    return expense.date.getFullYear().toString() === filteredYear
-  })
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
     
   return (
@@ -26,6 +27,7 @@ const Expenses = (props) => {
      /* {filteredExpenses.length === 0 && <p>No Expenses Found</p> }
      {filteredExpenses.length > 0 && filteredExpenses.map((expense) => 
      <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>)} */}
+    <ExpensesChart expenses={filteredExpenses}/>
     <ExpensesList items ={filteredExpenses} />
     </Card>
     </div>
